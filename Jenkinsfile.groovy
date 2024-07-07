b5a66aab517921748f63c4ebc0e9f695d8be2ffc
dvirlabs <dvirlabs@gmail.com>
2024-07-08 01:47:13 +0300

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
     stage('Pull base image') {
       steps {
         // sh "docker login -u dvirlabs -p ${DOCKER_TOKEN}"
-        docker.image('dvirlabs/jenkins-httpd:v1').pull()
+        docker.image('dvirlabs/jenkins-httpd:v1').pull() 
       }
     }
     stage('Build new image') {
